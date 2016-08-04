2777d3c379cda874fce9595ff047da5bff01e416
Pouya Poorrahman <poyaskysps@yahoo.com>
2016-08-05 02:02:35 +0430

    Jove V2.0

--- plugins/rmsg2.lua
+++ plugins/rmsg2.lua
@@ -1,11 +1,22 @@
+--[[
+|------------------------------------------------- |--------- ______-----------------_________---|
+|   ______   __   ______    _____     _____    __  |  _____  |  ____|  __     __    /  _______/  |
+|  |__  __| |  | |__  __|  /     \   |     \  |  | | |__   | | |____  |  |   |  |  /  /______    |
+|    |  |   |  |   |  |   /  /_\  \  |  |\  \ |  | |   /  /  |  ____| |  |   |  |  \______   /   |
+|    |  |   |  |   |  |  /  _____  \ |  | \  \|  | |  /  /_  | |____  |  |___|  |   _____/  /    |
+|    |__|   |__|   |__| /__/     \__\|__|  \_____| | |_____| |______|  \_______/  /________/     |
+|--------------------------------------------------|---------------------------------------------|
+|  This Project Powered by : Pouya Poorrahman CopyRight 2016 Jove Version 2.0 Anti Spam Cli Bot  |
+|------------------------------------------------------------------------------------------------|
+]]
 local function history(extra, suc, result)
   for i=1, #result do
     delete_msg(result[i].id, ok_cb, false)
   end
   if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'" recently supergroup messages removed', ok_cb, false)
+    send_msg(extra.chatid, '"'..#result..'" recently supergroup messages removed🔸', ok_cb, false)
   else
-    send_msg(extra.chatid, 'Your number of messages removed', ok_cb, false)
+    send_msg(extra.chatid, 'Your number of messages removed🔸', ok_cb, false)
   end
 end
 local function run(msg, matches)
