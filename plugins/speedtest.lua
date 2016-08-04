ec54c8270f83c3ded0432816ed1de85cd9595b9d
Pouya Poorrahman <poyaskysps@yahoo.com>
2016-08-04 21:58:11 +0430

    Jove V2.0

---
+++ plugins/speedtest.lua
@@ -0,0 +1,14 @@
+function run(msg, matches)
+if not is_sudo(msg) then
+return 
+end
+text = io.popen("speedtest-cli "):read('*all')
+  return text
+end
+return {
+  patterns = {
+    '^speedtest$'
+  },
+  run = run,
+  moderated = true
+}
