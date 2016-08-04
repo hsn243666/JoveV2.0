2806dc4eb9955d0fe7dab087b8a8b5d2b81ba375
Pouya Poorrahman <poyaskysps@yahoo.com>
2016-08-05 02:00:37 +0430

    Jove V2.0

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -37,17 +37,17 @@ local function list_all_plugins(only_enabled)
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
     --  ✅ enabled, ⛔️ disabled
-    local status = '⛔️'
+    local status = '🔹️|Deactive|'
     nsum = nsum+1
     nact = 0
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '✅' 
+        status = '🔸|Active|' 
       end
       nact = nact+1
     end
-    if not only_enabled or status == '✅' then
+    if not only_enabled or status == '🔸|Active|' then
       -- get the name
       v = string.match (v, "(.*)%.lua")
       text = text..nsum..'. '..v..'  '..status..'\n'
@@ -62,17 +62,17 @@ local function list_plugins(only_enabled)
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
     --  ✅ enabled, ⛔️ disabled
-    local status = '⛔️'
+    local status = '🔹️|Deactive|
     nsum = nsum+1
     nact = 0
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '✅' 
+        status = '🔸|Active|'
       end
       nact = nact+1
     end
-    if not only_enabled or status == '✅' then
+    if not only_enabled or status == '🔸|Active|' then
       -- get the name
       v = string.match (v, "(.*)%.lua")
       text = text..v..'  '..status..'\n'
