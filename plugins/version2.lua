83fdf15db17f4f67d3d3976ba2542eb9358e396d
Pouya Poorrahman <poyaskysps@yahoo.com>
2016-08-05 13:28:56 +0430

    Jove V2.0

--- plugins/version2.lua
+++ plugins/version2.lua
@@ -1,23 +1,36 @@
+--[[
+|------------------------------------------------- |--------- ______-----------------_________---|
+|   ______   __   ______    _____     _____    __  |  _____  |  ____|  __     __    /  _______/  |
+|  |__  __| |  | |__  __|  /     \   |     \  |  | | |__   | | |____  |  |   |  |  /  /______    |
+|    |  |   |  |   |  |   /  /_\  \  |  |\  \ |  | |   /  /  |  ____| |  |   |  |  \______   /   |
+|    |  |   |  |   |  |  /  _____  \ |  | \  \|  | |  /  /_  | |____  |  |___|  |   _____/  /    |
+|    |__|   |__|   |__| /__/     \__\|__|  \_____| | |_____| |______|  \_______/  /________/     |
+|--------------------------------------------------|---------------------------------------------|
+|  This Project Powered by : Pouya Poorrahman CopyRight 2016 Jove Version 2.0 Anti Spam Cli Bot  |
+|------------------------------------------------------------------------------------------------|
+]]
 do
 
 function run(msg, matches)
   return [[ 
-🔰Version:Jove (Old Smart) V 1.3🔰
+🔰Version:Jove (Old Smart) V 2.0🔰
 🔰A bot based on TeleSeed (Smart،Fast،Powerful)🔰
 🔰CPU Model:Intel(R) Core(TM) i7-6700 CPU @ 3.40GHz🔰
-🔰Number of cores:12 cores🔰
-🔰Ram:14 Gig🔰
-🔰HDD:120 Gig SSD🔰
+🔰Number of cores:12 cores-2 cores support🔰
+🔰Ram:14 Gig-2 Gig support🔰
+🔰HDD:120 Gig SSD-60 Gig support🔰
 🔰Server Location:Hetzaner Germany🔰
 🔰Developer and Sudo: @vVv_ERPO_vVv🔰
 🔰Developer and Sudo: @vWv_ERPO_vWv🔰
 🔰Channel: @JoveCH🔰
+🔰Helper: @JoveTGBot🔰
 🔰Other Robots🔰:
- #Smart , #Ruling , #Savior , ...
+ @Smart_TG , @RulingTG , @Savior_Team , ...
 🇮🇷🇮🇷🇮🇷🇮🇷🇮🇷🇮🇷🇮🇷🇮
 ☀️Special Tnx☀️:
 @Us_Mm MamadReza
 @Jove_TG_bot Mamad
+@mostafa_028 Mostafa
 and other people...
 🇮🇷🇮🇷🇮🇷🇮🇷🇮🇷🇮🇷🇮🇷🇮
 ☀️☀️
