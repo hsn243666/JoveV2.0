bae25b93b859615d0ad9fdbb20f5f4f397a24d07
Pouya Poorrahman <poyaskysps@yahoo.com>
2016-08-05 14:32:41 +0430

    Jove V2.0

--- steady.sh
+++ steady.sh
@@ -43,7 +43,7 @@ cat << EOF
  
 EOF
 echo -e "                \e[100m                Steady script           \e[00;37;40m"
-echo -e "               \e[01;34m                    by iicc                \e[00;37;40m"
+echo -e "               \e[01;34m             by Titan Zeus Team         \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
