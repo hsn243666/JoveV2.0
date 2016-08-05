2136c3cf7ab5acca108ed43394dcf366218bd143
Pouya Poorrahman <poyaskysps@yahoo.com>
2016-08-05 13:20:53 +0430

    Jove V2.0

--- plugins/support2.lua
+++ plugins/support2.lua
@@ -1,7 +1,18 @@
+--[[
+|------------------------------------------------- |--------- ______-----------------_________---|
+|   ______   __   ______    _____     _____    __  |  _____  |  ____|  __     __    /  _______/  |
+|  |__  __| |  | |__  __|  /     \   |     \  |  | | |__   | | |____  |  |   |  |  /  /______    |
+|    |  |   |  |   |  |   /  /_\  \  |  |\  \ |  | |   /  /  |  ____| |  |   |  |  \______   /   |
+|    |  |   |  |   |  |  /  _____  \ |  | \  \|  | |  /  /_  | |____  |  |___|  |   _____/  /    |
+|    |__|   |__|   |__| /__/     \__\|__|  \_____| | |_____| |______|  \_______/  /________/     |
+|--------------------------------------------------|---------------------------------------------|
+|  This Project Powered by : Pouya Poorrahman CopyRight 2016 Jove Version 2.0 Anti Spam Cli Bot  |
+|------------------------------------------------------------------------------------------------|
+]]
 do
 
 function run(msg, matches)
-  return " 🔰Way of Support🔰 : \n🔰HelperBot: @JoveTGBot\n🔰Sudo and Manager: @vVv_ERPO_vVv\n🔰Sudo2 and Manager: @vWv_ERPO_vWv\n🔰Channel: @JoveCH"
+  return " 🔰Way of Support🔰 :\n🔰Support Link: https://telegram.me/joinchat/DQ3RTj8dfYTlfXzNJ4Q16g\n🔰HelperBot: @JoveTGBot\n🔰Sudo and Manager: @vVv_ERPO_vVv\n🔰Sudo2 and Manager: @vWv_ERPO_vWv\n🔰Channel: @JoveCH"
   end
 return {
   description = "shows support link", 
