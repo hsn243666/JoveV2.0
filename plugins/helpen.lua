23bb1e9c2421d1ea6b0b757c55991a812728f830
Pouya Poorrahman <poyaskysps@yahoo.com>
2016-08-25 14:17:58 +0430

    Jove V3.1

--- plugins/helpen.lua
+++ plugins/helpen.lua
@@ -2,57 +2,71 @@ do
 
 function run(msg, matches)
   return [[ 
-🔰Jove Bot English Help:🔰
-🌟Customization SuperGroup:🌟
-🔸lock
-(links-contacts-spam-arabic-member-rtl-tgservice-sticker-tag-emoji-english-fwd-reply-join-username-media-fosh-bots-operator-etehad-all)
-🔹unlock
-(links-contacts-spam-arabic-member-rtl-tgservice-sticker-tag-emoji-english-fwd-reply-join-username-media-fosh-bots-operator-etehad-all)
-🔸mute
+💠Jove English Help V3.1💠
+
+🔆Customization SuperGroup:🔆
+💭lock
+(links-contacts-spam-arabic-member-rtl-tgservice-sticker-tag-emoji-english-forward-badword-webpage)
+🗯unlock
+(links-contacts-spam-arabic-member-rtl-tgservice-sticker-tag-emoji-english-forward-badword-webpage)
+
+📉mute
 (audio-video-photo-gifs-text-all)
-🔹unmute
+📈unmute
 (audio-video-photo-gifs-text-all)
-🔻set
+
+📝set
 (photo-name-about-rules-username-flood)
-🔸public (yes-no)
-🔘Manage a User🔘:
-🔺block (Id or Reply)
-🔺kick (Id or Reply)
-🔺ban (Id or Reply)
-🔻unban (Id or Reply)
-🔸setowner (Id or Reply)
-🔹promote (Id or Reply)
-🔸demote (Id or Reply)
-🔹setadmin (Id or Reply)
-🔸demoteadmin (Id or Reply)
-🔺silent (Id or Reply)
+
+🚩public (🔓-🔐)
+
+💥Manage a User💥:
+🚫block (Id or Reply)
+🚫kick (Id or Reply)
+🚫ban (Id or Reply)
+
+⭕️unban (Id or Reply)
+
+📍setowner (Id or Reply)
+📍setadmin (Id or Reply)
+📍promote (Id or Reply)
+
+🔻demote (Id or Reply)
+🔻demoteadmin (Id or Reply)
+
+📵silent (Id or Reply)
 (If two times send this word the silent user will be free)
-💭Public Supergroup Behest:💭
-🔹list
+
+📚Public Supergroup Behest:📚
+▫️list
 (member-silent-mutes-admins)
-🔸settings
-🔹rules
+▪️admins
+▫️owner
+▪️bots
+
+🔹settings
 🔸gpinfo
-🔹admins
-🔸del (reply)
-🔹owner
-🔸bots
-🔹id
-🔸kickme
-🔹setlink
-🔸link
-🔹res (id)
+
+🔻rules
+
+♦️del (reply)
+♦️kickme
+
+🌀res (id)
+🌀id
+
+🔏setlink
+🏳link
 〰〰〰〰〰〰〰
-☑️for get other behest plz send tools parameter
-©#JoveTG V1.3
-♦️Zeus Team♦️
+☑️ #Jove V3.1
+💠Titan Zeus💠
 ]]
 end
 
 return {
   patterns = {
     "^help$",
-    "^guide$"
+    "^[/#!]help$"
   }, 
   run = run 
 }
